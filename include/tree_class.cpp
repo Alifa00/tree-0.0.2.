45552b55969a113c2d43ca0ba3b8cbfdfe465b7d
Vladimir Alefirov <31669425+Alifa00@users.noreply.github.com>
2018-05-14 17:01:15 +0300

    Update tree_class.cpp

--- include/tree_class.cpp
+++ include/tree_class.cpp
@@ -16,53 +16,53 @@ private:
             delete right;
         }
         void insertrec(T key) {
-            if (this - > key > key) {
-                if (this - > left != nullptr) {
-                    this - > left - > insertrec(key);
+            if (this -> key > key) {
+                if (this -> left != nullptr) {
+                    this -> left -> insertrec(key);
                 }
                 else {
-                    this - > left = new node_t();
-                    this - > left - > key = key;
+                    this -> left = new node_t();
+                    this -> left -> key = key;
                 }
             }
-            if (this - > key < key) {
-                if (this - > right != nullptr) {
-                    this - > right - > insertrec(key);
+            if (this -> key < key) {
+                if (this -> right != nullptr) {
+                    this -> right -> insertrec(key);
                 }
                 else {
-                    this - > right = new node_t();
-                    this - > right - > key = key;
+                    this -> right = new node_t();
+                    this -> right -> key = key;
                 }
             }
             return;
         }
         void printrec(std::ostream & stream, int minus_num) const {
-            if (this - > right != nullptr) {
-                this - > right - > printrec(stream, minus_num + 4);
+            if (this -> right != nullptr) {
+                this -> right -> printrec(stream, minus_num + 4);
             }
             for (int i = 0; i < minus_num + 4; i++) {
                 stream << '-';
             }
             stream << key << '\n';
-            if (this - > left != nullptr) {
-                this - > left - > printrec(stream, minus_num + 4);
+            if (this -> left != nullptr) {
+                this -> left -> printrec(stream, minus_num + 4);
             }
             return;
         }
         bool findrec(T key) const {
-            if (this - > key > key) {
-                if (this - > left != nullptr) {
-                    return this - > left - > findrec(key);
+            if (this -> key > key) {
+                if (this -> left != nullptr) {
+                    return this -> left -> findrec(key);
                 }
                 return false;
             }
-            if (this - > key < key) {
-                if (this - > right != nullptr) {
-                    return this - > right - > findrec(key);
+            if (this -> key < key) {
+                if (this -> right != nullptr) {
+                    return this -> right -> findrec(key);
                 }
                 return false;
             }
-            if (this - > key == key) {
+            if (this -> key == key) {
                 return true;
             }
             return false;
@@ -90,72 +90,71 @@ private:
             return false;
         }
         bool removerec(T key) {
-            if (this - > key > key) {
-                if (this - > left != nullptr) {
-                    if (this - > left - > right == nullptr && this -> left -> left == nullptr && this - > left -> key == key) {
-                        delete this - > left;
-                        this - > left = nullptr;
+            if (this -> key > key) {
+                if (this -> left != nullptr) {
+                    if (this -> left -> right == nullptr && this -> left -> left == nullptr && this -> left -> key == key) {
+                        delete this -> left;
+                        this -> left = nullptr;
                         return true;
                     }
-                    return this - > left - > removerec(key);
+                    return this -> left -> removerec(key);
                 }
                 else {
                     return false;
                 }
             }
-            if (this - > key < key) {
-                if (this - > right != nullptr) {
-                    if (this - > right - > right == nullptr && this -> right - > left == nullptr && this - >right - > key == key) {
-                        delete this - > right;
-                        this - > right = nullptr;
+            if (this -> key < key) {
+                if (this -> right != nullptr) {
+                    if (this -> right -> right == nullptr && this -> right -> left == nullptr && this ->right -> key == key) {
+                        delete this -> right;
+                        this -> right = nullptr;
                         return true;
                     }
-                    return this - > right - > removerec(key);
+                    return this -> right -> removerec(key);
                 }
                 else {
                     return false;
                 }
             }
-            if (this - > key == key) {
-                if (this - > left == nullptr && this - > right !=
+            if (this -> key == key) {
+                if (this -> left == nullptr && this -> right !=
                     nullptr) {
-                    this - > key = this - > right - > key;
-                    node_t * hp = this - > right;
-                    this - > left = this - > right - > left;
-                    this - > right = this - > right - > right;
-                    hp - > left = nullptr;
-                    hp - > right = nullptr;
+                    this -> key = this -> right -> key;
+                    node_t * hp = this -> right;
+                    this -> left = this -> right -> left;
+                    this -> right = this -> right -> right;
+                    hp -> left = nullptr;
+                    hp -> right = nullptr;
                     delete hp;
                     return true;
                 }
-                if (this - > right == nullptr && this - > left !=
+                if (this -> right == nullptr && this -> left !=
                     nullptr) {
-                    this - > key = this - > left - > key;
-                    node_t * hp = this - > left;
-                    this - > right = this - > left - > right;
-                    this - > left = this - > left - > left;
-                    hp - > left = nullptr;
-                    hp - > right = nullptr;
+                    this -> key = this -> left -> key;
+                    node_t * hp = this -> left;
+                    this -> right = this -> left -> right;
+                    this -> left = this -> left -> left;
+                    hp -> left = nullptr;
+                    hp -> right = nullptr;
                     delete hp;
                     return true;
                 }
-                if (this - > right != nullptr && this - > left !=
+                if (this -> right != nullptr && this -> left !=
                     nullptr) {
-                    node_t * premostleft = this - > right;
-                    if (premostleft - > left == nullptr) {
-                        this - > key = premostleft - > key;
-                        this - > right = premostleft - > right;
-                        premostleft - > left = nullptr;
-                        premostleft - > right = nullptr;
+                    node_t * premostleft = this -> right;
+                    if (premostleft -> left == nullptr) {
+                        this -> key = premostleft -> key;
+                        this -> right = premostleft -> right;
+                        premostleft -> left = nullptr;
+                        premostleft -> right = nullptr;
                         delete premostleft;
                         return true;
                     }
-                    while (premostleft - > left - > left != nullptr) {
-                        premostleft = premostleft - > left;
+                    while (premostleft -> left -> left != nullptr) {
+                        premostleft = premostleft -> left;
                     }
-                    this - > key = premostleft - > left - > key;
-                    return this - > right - > removerec(this - >
-                                                        key);
+                    this -> key = premostleft -> left -> key;
+                    return this -> right -> removerec(this ->key);
                 }
             }
             return false;
@@ -182,14 +181,14 @@ template <typename T>
 tree_t <T> ::tree_t(std::initializer_list < T > keys) {
     root_ = nullptr;
     for (T i: keys) {
-        this - > insert(i);
+        this -> insert(i);
     }
 }
 template <typename T>
 void tree_t <T> ::insert(T key) {
     if (root_ == nullptr) {
         root_ = new node_t;
-        root_ - > key = key;
+        root_ -> key = key;
         return;
     }
     root_ -> insertrec(key);
@@ -198,14 +197,14 @@ void tree_t <T> ::insert(T key) {
 template <typename T>
 void tree_t <T> ::print(std::ostream & stream) const {
     if (root_ != nullptr) {
-        root_ - > printrec(stream, 0);
+        root_ -> printrec(stream, 0);
     }
     return;
 }
 template <typename T>
 bool tree_t <T> ::find(T key) const {
     if (root_ != nullptr) {
-        return root_ - > findrec(key);
+        return root_ -> findrec(key);
     }
     return false;
 }
@@ -221,12 +220,10 @@ auto tree_t <T> ::operator == (tree_t <T>const & other) const {
 }
 template <typename T>
 bool tree_t <T> ::remove(T key) {
-    if (root_ != nullptr && (root_ - > left != nullptr || root_ - > right !=
-                             nullptr)) {
+    if (root_ != nullptr && (root_ - > left != nullptr || root_ -> right !=nullptr)) {
         return root_ - > removerec(key);
     }
-    if (root_ != nullptr && root_ - > left == nullptr && root_ - > right ==
-        nullptr) {
+    if (root_ != nullptr && root_ - > left == nullptr && root_ -> right ==nullptr) {
         delete root_;
         root_ = nullptr;
         return true;
